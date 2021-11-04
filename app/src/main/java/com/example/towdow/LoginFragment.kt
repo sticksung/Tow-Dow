15e2b8ac98a0b1a2f1fad6dce24074120b277e13
nicolefitz <nicolef00@vt.edu>
2021-11-04 15:01:17 -0400

    Colors and navigation for login

--- app/src/main/java/com/example/towdow/LoginFragment.kt
+++ app/src/main/java/com/example/towdow/LoginFragment.kt
@@ -5,19 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.example.towdow.databinding.LoginFragmentBinding
 
 
 class LoginFragment : Fragment() {
+    private var _binding: LoginFragmentBinding? =null
+    private val binding get() =_binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.login_fragment, container, false)
+        _binding = LoginFragmentBinding.inflate(inflater,container, false)
+        val v = binding.root
 
 
-        return view
+        binding.towDowImage.setImageResource(R.drawable.logo)
+        binding.signinButton.setOnClickListener{
+            view?.findNavController()?.navigate(R.id.action_loginFragment_to_homeFragment)
+        }
+
+        binding.signupButton.setOnClickListener{
+            view?.findNavController()?.navigate(R.id.action_loginFragment_to_signupFragment)
+        }
+        return v
     }
 
 
 }
+
